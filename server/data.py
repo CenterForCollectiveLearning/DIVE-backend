5287b518cb5451c86f2c4ce1f7083cc229b8705f
Kevin Hu <kzh@Kevins-MacBook-Air-2.local>
2014-05-08 18:54:38 -0400

    Get back Treemap data as you change the sped

--- server/data.py
+++ server/data.py
@@ -8,13 +8,15 @@ class DAL(object):
     def __init__(self):
         self.dataset_ids = {}
         self.column_ids = {}
+        self.column_id_to_name = {}
         self.num_datasets = 0
         return
 
     def insert_dataset(self, path):
         if path not in self.dataset_ids:
             self.dataset_ids[path] = self.num_datasets
-            self.column_ids[path] = self.get_column_ids(path)
+            self.column_ids[path] = self.get_column_ids(path).keys()
+            self.column_id_to_name[path] = self.get_column_ids(path)
             self.num_datasets += 1
         return
 
@@ -25,9 +27,12 @@ class DAL(object):
         return
 
     def get_column_name_from_id(self, passed_dataset_id, passed_column_id):
-        for path, stored_id in self.column_ids.iteritems():
-            if stored_id == passed_id:
-                return path
+        print "PASSED DS ID", "PASSED COL ID"
+        print passed_dataset_id, passed_column_id
+        for path, stored_dataset_id in self.dataset_ids.iteritems():
+            if stored_dataset_id == passed_dataset_id:
+                print "MATCH ID", path
+                return self.column_id_to_name[path][passed_column_id]
         return
 
     def get_dataset_id(self, path):
@@ -45,7 +50,7 @@ class DAL(object):
         delim = get_delimiter(path)
         l = f.readline().split(delim)
 
-        return [i for i in range(0, len(l))]
+        return dict([(i, name) for i, name in enumerate(l)])
 
 # A singleton object
 DAL = DAL()
