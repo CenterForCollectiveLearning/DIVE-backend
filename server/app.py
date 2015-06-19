10da8b93c0f45fa4c5b3dc727480ef0d6339e936
Kevin Hu <kzh@mit.edu>
2015-06-19 12:48:39 -0400

    Renaming assemble -> export

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 import os
+from api.api import app
+from flask import request
 from flask import Flask
 from config import config
 
@@ -16,3 +18,63 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 STATIC_FOLDER = os.path.join(os.curdir, config['STATIC_FOLDER'])
 app.static_folder = STATIC_FOLDER
+
+
+@app.before_request
+def option_autoreply():
+    """ Always reply 200 on OPTIONS request """
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        print "Here comes an OPTIONS request"
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
+
+@app.after_request
+def replace_nan(resp):
+    print resp
+    try:
+        cleaned_data = resp.get_data().replace('nan', 'null').replace('NaN', 'null')
+        resp.set_data(cleaned_data)
+        return resp
+    except:
+        return resp
+
+
+@app.after_request
+def set_allow_origin(resp):
+    """ Set origin for GET, POST, PUT, DELETE requests """
+
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if request.method != 'OPTIONS' and 'Origin' in request.headers:
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+    return resp
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
