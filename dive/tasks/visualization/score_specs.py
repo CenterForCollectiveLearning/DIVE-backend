155bc15eb47a44ff266d76f9d6c240cf4d496f35
kevinzenghu <kzh@mit.edu>
2015-10-25 19:05:36 -0400

    Calculating scores again

--- dive/tasks/visualization/score_specs.py
+++ dive/tasks/visualization/score_specs.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy.stats import entropy, normaltest, mode, pearsonr, linregress
 from dive.tasks.visualization import GeneratingProcedure, TypeStructure
 
 import logging
@@ -7,6 +9,7 @@ logger = logging.getLogger(__name__)
 def get_expressiveness(spec):
     result = {}
     return result
+
 
 # Effectiveness
 def get_effectiveness(spec):
@@ -24,34 +27,39 @@ def gini(list_of_values):
   return (fair_area - area) / fair_area
 
 
-import numpy as np
-from scipy.stats import entropy, normaltest, mode
 def _mode(v):
     m = mode(v)
     return [ m[0][0], m[1][0] ]
 
 
+def _normaltest(v):
+    return normaltest(v).pvalue
+
+
 # Uniformity: Chi-Sq test or KL test against uniform distribution?
 univariate_tests = {
-    # 'gini': gini,
-    # 'entropy': entropy,  # Shannon entropy, base e
-    # # 'normality': normaltest,  # Requires at least n >= 8
+    'gini': gini,
+    'entropy': entropy,  # Shannon entropy, base e
+    'normality': _normaltest,  # Requires at least n >= 8
+    'variance': np.var,
+    'size': len,
     # 'median': np.median,
     # 'average': np.average,
     # 'std': np.std,
-    # 'variance': np.var,
     # 'maximum': np.max,
     # 'minimum': np.min,
     # 'mode': _mode,
-    'size': len,
 }
 
 
-from scipy.stats import pearsonr, linregress
+def _correlation(v1, v2):
+    print pearsonr(v1, v2)
+    return pearsonr(v1, v2)[0]
+
 # Two quantitative variables
 bivariate_tests = {
-    'correlation': pearsonr,
-    'linearRegression': linregress
+    'correlation': _correlation
+    # 'linearRegression': linregress
 }
 
 # Statistical
@@ -72,22 +80,32 @@ def get_statistical_properties(data, gp, ts):
         if v:
             for test_name, test_fn in univariate_tests.iteritems():
                 try:
-                    stats[test_name] = test_fn(v)
+                    test_value = test_fn(v)
+                    if np.isnan(test_value) or np.isinf(test_value):
+                        test_value = None
+                    stats[test_name] = test_value
                 except:
-                    logger.error('Failed scoring for test %s, gp %s, ts %s', test_name, gp, ts)
+                    stats[test_name] = None
+                    logger.debug('Failed scoring for test %s, gp %s, ts %s', test_name, gp, ts)
                     continue
-    if ts in [TypeStructure.Q_Q]:
-        v = None
-        if gp == GeneratingProcedure.AGG_AGG.value:
-            v1 = data.get('fieldA')
-            v2 = data.get('fieldB')
-        if gp == GeneratingProcedure.VAL_VAL.value:
-            v1 = data.get('fieldA')
-            v2 = data.get('fieldB')
-        if v:
-            for test_name, test_fn in bivariate_tests.iteritems():
-                stats[test_name] = test_fn(v1, v2)
 
+    logger.info(ts)
+    if ts in [TypeStructure.Q_Q.value]:
+        logger.info('Conducting bivariate test')
+        logger.info(data)
+        v1 = data.get('fieldA')
+        v2 = data.get('fieldB')
+        if v1 and v2:
+            for test_name, test_fn in bivariate_tests.iteritems():
+                try:
+                    test_value = test_fn(v1, v2)
+                    if np.isnan(test_value) or np.isinf(test_value):
+                        test_value = None
+                    stats[test_name] = test_value
+                except:
+                    stats[test_name] = None
+                    logger.debug('Failed scoring for test %s, gp %s, ts %s', test_name, gp, ts)
+                    continue
     return stats
 
 
