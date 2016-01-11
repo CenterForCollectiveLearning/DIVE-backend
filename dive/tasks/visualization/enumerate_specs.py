fa96d41ccdc05f08330de57983ab1596705a04d4
kevinzenghu <kzh@mit.edu>
2016-01-11 12:46:19 -0500

    More refactoring

--- dive/tasks/visualization/enumerate_specs.py
+++ dive/tasks/visualization/enumerate_specs.py
@@ -15,66 +15,29 @@ def enumerate_viz_specs(project_id, dataset_id, selected_fields):
     '''
     TODO Move key filtering to the db query
     TODO Incorporate 0D and 1D data returns
-    TODO Use IDs instead of names for fields
     '''
     specs = []
 
     # Get field properties
     with task_app.app_context():
-        field_properties = db_access.get_field_properties(project_id, dataset_id)
-        new_field_properties = []
-        for field in field_properties:
-            desired_keys = ['is_id', 'is_unique', 'general_type', 'type', 'name', 'id']
-            new_field = { k: field[k] for k in desired_keys }
-            new_field_properties.append(new_field)
-        field_properties = new_field_properties
+        desired_keys = ['is_id', 'is_unique', 'general_type', 'type', 'name', 'id']
+        raw_field_properties = db_access.get_field_properties(project_id, dataset_id)
+        field_properties = [{ k: field[k] for k in desired_keys } for field in raw_field_properties]
+
+    logger.info('Number of fields: %s', len(field_properties))
 
     if selected_fields:
-        selected_field_docs = []
-        c_fields = []
-        c_fields_not_selected = []
-        q_fields = []
-        q_fields_not_selected = []
-
-        n_c = 0
-        n_q = 0
-        n_c_total = 0
-        n_q_total = 0
-
-        for field in field_properties:
-            is_selected_field = next((selected_field for selected_field in selected_fields if selected_field['field_id'] == field['id']), None)
-            if is_selected_field:
-                selected_field_docs.append(field)
-
-            general_type = field['general_type']
-            if general_type == 'c':
-                if is_selected_field:
-                    n_c = n_c + 1
-                    c_fields.append(field)
-                else:
-                    c_fields_not_selected.append(field)
-                n_c_total = n_c_total + 1
-
-            elif general_type == 'q':
-                if is_selected_field:
-                    n_q = n_q + 1
-                    q_fields.append(field)
-                else:
-                    q_fields_not_selected.append(field)
-                n_q_total = n_q_total + 1
-
-        logger.info('n_c = %s | n_q = %s', n_c, n_q)
-        logger.info('c_fields %s', [f['name'] for f in c_fields])
-        logger.info('q_fields %s', [f['name'] for f in q_fields])
+        selected_field_docs, c_fields, c_fields_not_selected, q_fields, q_fields_not_selected = \
+            get_selected_fields(field_properties, selected_fields)
 
         baseline_specs = get_baseline_viz_specs(selected_field_docs)
         specs.extend(baseline_specs)
 
-        cascading_viz_specs = get_cascading_viz_specs(n_c, n_q, c_fields, q_fields)
+        cascading_viz_specs = get_cascading_viz_specs(c_fields, q_fields, c_fields_not_selected, q_fields_not_selected)
         specs.extend(cascading_viz_specs)
 
-        expanded_viz_specs = get_expanded_viz_specs(n_c, n_q, c_fields, q_fields)
-        specs.extend(expanding_viz_specs)
+        expanded_viz_specs = get_expanded_viz_specs(c_fields, q_fields, c_fields_not_selected, q_fields_not_selected)
+        specs.extend(expanded_viz_specs)
     else:
         baseline_specs = get_baseline_viz_specs(field_properties)
         specs.extend(baseline_specs)
@@ -90,12 +53,73 @@ def enumerate_viz_specs(project_id, dataset_id, selected_fields):
     return specs
 
 
-def get_cascading_viz_specs(n_c, n_q, c_fields, q_fields):
+def get_baseline_viz_specs(field_properties):
+    '''
+    Single-field summary visualizations
+    '''
+    specs = []
+    for field in field_properties:
+        # Skip unique fields
+        if field['is_id']: continue
+        if field['is_unique']: continue
+
+        general_type = field['general_type']
+        if general_type == 'c':
+            C_specs = C(field)
+            specs.extend(C_specs)
+        elif general_type == 'q':
+            A_specs = A(field)
+            specs.extend(A_specs)
+        else:
+            raise ValueError('Not valid general_type', general_type)
+    logger.info('Got %s baseline specs', len(specs))
+    return specs
+
+
+def get_selected_fields(field_properties, selected_fields):
+    '''
+    Get selected fields, selected Q and C fields, and non-selected Q and C fields
+    '''
+    selected_field_docs = []
+    c_fields = []
+    c_fields_not_selected = []
+    q_fields = []
+    q_fields_not_selected = []
+
+
+    for field in field_properties:
+        is_selected_field = next((selected_field for selected_field in selected_fields if selected_field['field_id'] == field['id']), None)
+        if is_selected_field:
+            selected_field_docs.append(field)
+
+        general_type = field['general_type']
+        if general_type == 'c':
+            if is_selected_field:
+                c_fields.append(field)
+            else:
+                c_fields_not_selected.append(field)
+
+        elif general_type == 'q':
+            if is_selected_field:
+                q_fields.append(field)
+            else:
+                q_fields_not_selected.append(field)
+
+    logger.info('n_c = %s | n_q = %s', len(c_fields), len(q_fields))
+    logger.info('c_fields %s', [f['name'] for f in c_fields])
+    logger.info('q_fields %s', [f['name'] for f in q_fields])
+    return selected_field_docs, c_fields, c_fields_not_selected, q_fields, q_fields_not_selected
+
+
+def get_cascading_viz_specs(c_fields, q_fields, c_fields_not_selected, q_fields_not_selected):
     '''
     Marginal and cascading visualization cases (excluding single-field cases)
     Essentially treating selection like a mini dataset
     '''
     specs = []
+    n_c = len(c_fields)
+    n_q = len(q_fields)
+
     if (n_c == 0):
         if (n_q > 1):
             B_specs = B(q_fields)
@@ -127,11 +151,14 @@ def get_cascading_viz_specs(n_c, n_q, c_fields, q_fields):
     return specs
 
 
-def get_expanded_viz_specs(n_c, n_q, c_fields, q_fields):
+def get_expanded_viz_specs(c_fields, q_fields, c_fields_not_selected, q_fields_not_selected):
     '''
     Expanded visualization cases (e.g. including fields not provided in arguments)
     '''
     specs = []
+    n_c = len(c_fields)
+    n_q = len(q_fields)
+
     for c_field_1 in c_fields:
         # Pairs of C fields
         for c_field_2 in c_fields_not_selected:
@@ -150,27 +177,4 @@ def get_expanded_viz_specs(n_c, n_q, c_fields, q_fields):
             D_specs = D(c_field, q_field_1)
             specs.extend(D_specs)
     logger.info('Got %s expanded specs', len(specs))
-    return specs
-
-
-def get_baseline_viz_specs(field_properties):
-    '''
-    Single-field summary visualizations
-    '''
-    specs = []
-    for field in field_properties:
-        # Skip unique fields
-        if field['is_id']: continue
-        if field['is_unique']: continue
-
-        general_type = field['general_type']
-        if general_type == 'c':
-            C_specs = C(field)
-            specs.extend(C_specs)
-        elif general_type == 'q':
-            A_specs = A(field)
-            specs.extend(A_specs)
-        else:
-            raise ValueError('Not valid general_type', general_type)
-    logger.info('Got %s baseline specs', len(specs))
     return specs
