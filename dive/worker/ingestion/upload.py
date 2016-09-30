0d3d131ea05154b12514b3765c3795f0cf197261
kevinzenghu <kzh@mit.edu>
2016-09-30 09:48:50 -0400

    Handled merge issues

--- dive/worker/ingestion/upload.py
+++ dive/worker/ingestion/upload.py
@@ -19,81 +19,8 @@ from dive.worker.core import celery, task_app
 from dive.base.data.access import get_data
 from dive.base.data.in_memory_data import InMemoryData as IMD
 
-from boto.s3.cors import CORSConfiguration
-from boto.exception import S3ResponseError
-
 import logging
 logger = logging.getLogger(__name__)
-
-def enable_bucket_cors(bucket):
-    """ For direct upload to work, the bucket needs to enable
-    cross-origin request scripting. """
-    try:
-        cors_cfg = bucket.get_cors()
-    except S3ResponseError:
-        cors_cfg = CORSConfiguration()
-    rules = [r.id for r in cors_cfg]
-    changed = False
-    if 'spendb_put' not in rules:
-        cors_cfg.add_rule(['PUT', 'POST'], '*',
-                          allowed_header='*',
-                          id='spendb_put',
-                          max_age_seconds=3000,
-                          expose_header='x-amz-server-side-encryption')
-        changed = True
-    if 'spendb_get' not in rules:
-        cors_cfg.add_rule('GET', '*', id='spendb_get')
-        changed = True
-
-    if changed:
-        bucket.set_cors(cors_cfg)
-
-
-def generate_s3_upload_policy(source, file_name, mime_type):
-    """ Generate a policy and signature for uploading a file directly to
-    the specified source on S3. """
-    obj = source._obj
-    if not hasattr(obj, 'key'):
-        return {
-            'status': 'error',
-            'message': 'Backend is not on S3, cannot generate signature.'
-        }
-
-    enable_bucket_cors(obj.store.bucket)
-    url = obj.key.generate_url(expires_in=0, force_http=True,
-                               query_auth=False)
-    url = url.split(obj.key.name)[0]
-
-    if 'https' in current_app.config.get('PREFERRED_URL_SCHEME'):
-        url = url.replace('http://', 'https://')
-
-    data = {
-        'url': url,
-        'status': 'ok',
-        'key': obj.key.name,
-        'source_name': source.name,
-        'aws_key_id': obj.store.aws_key_id,
-        'acl': 'public-read',
-        'file_name': file_name,
-        'mime_type': mime_type
-    }
-    expire = datetime.utcnow() + timedelta(days=7)
-    expire, ms = expire.isoformat().split('.')
-    policy = {
-        "expiration": expire + "Z",
-        "conditions": [
-            {"bucket": obj.store.bucket_name},
-            ["starts-with", "$key", data['key']],
-            {"acl": data['acl']}
-        ]
-    }
-
-    # data['raw_policy'] = json.dumps(policy)
-    data['policy'] = b64encode(json.dumps(policy))
-    data['signature'] = b64encode(hmac.new(obj.store.aws_secret,
-                                           data['policy'],
-                                           hashlib.sha1).digest())
-    return data
 
 
 def upload_file(project_id, file):
