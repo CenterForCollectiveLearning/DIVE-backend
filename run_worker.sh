deb6c61e6a65e45a6c02273ce7185af2c8f04a9e
kevinzenghu <kzh@mit.edu>
2016-10-02 18:32:30 -0400

    Update celery worker command.

--- run_worker.sh
+++ run_worker.sh
@@ -1,8 +1,7 @@
 celery worker \
   --app=dive.worker.core \
   --autoscale=10,3 \
-  --hostname worker1.%h
-  --loglevel INFO \
+  --loglevel=INFO \
   --without-gossip \
   --without-mingle \
   --without-heartbeat
