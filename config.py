de1ec46b78b64e4b335697e33141f8cfd858d4f2
kevinzenghu <kzh@mit.edu>
2016-10-07 17:03:06 -0400

    Move development environment onto remote services. Add path to feedback.

--- config.py
+++ config.py
@@ -37,21 +37,21 @@ class BaseConfig(object):
         AWS_DATA_BUCKET = env('DIVE_AWS_DATA_BUCKET')
         AWS_REGION = env('DIVE_AWS_REGION')
     PRELOADED_PATH = base_dir_path('preloaded')
-    ALEMBIC_DIR = base_dir_path('migrate')
 
     # DB
-    DATABASE_URI = 'admin:password@localhost/dive'
+    DATABASE_URI = '%s:%s@%s/%s' % (env('SQLALCHEMY_DATABASE_USER'), env('SQLALCHEMY_DATABASE_PASSWORD'), env('SQLALCHEMY_DATABASE_ENDPOINT'), env('SQLALCHEMY_DATABASE_NAME'))
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://%s' % DATABASE_URI
     SQLALCHEMY_POOL_SIZE=20
     SQLALCHEMY_MAX_OVERFLOW=100
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://%s' % DATABASE_URI
+    ALEMBIC_DIR = base_dir_path('migrate')
 
     # Worker
     CELERY_ALWAYS_EAGER = False
     CELERY_ACCEPT_CONTENT = [ 'pjson' ]
     CELERY_TASK_SERIALIZER = 'pjson'
     CELERY_RESULT_SERIALIZER = 'pjson'
-    CELERY_BROKER_URL = 'librabbitmq://admin:password@localhost/dive'
-    CELERY_RESULT_BACKEND = 'db+postgresql://%s' % DATABASE_URI
+    CELERY_BROKER_URL = env('DIVE_AMQP_URL', 'librabbitmq://admin:password@localhost/dive')
+    CELERY_RESULT_BACKEND =  'db+postgresql://%s' % DATABASE_URI
     CELERY_IMPORTS = []
     for root, dirs, files in walk("./dive/worker"):
         path = root.split('/')
@@ -60,6 +60,13 @@ class BaseConfig(object):
             if f.endswith('.py') and f != '__init__.py':
                 CELERY_IMPORTS.append('%s.%s' % (dir_path, f[:-3]))
 
+    BROKER_POOL_LIMIT = 1 # Will decrease connection usage
+    BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
+    BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
+    CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
+    CELERY_EVENT_QUEUE_EXPIRES = 60
+
+    # Result persistence
     RECOMPUTE_FIELD_PROPERTIES = True
     RECOMPUTE_VIZ_SPECS = True
     RECOMPUTE_STATISTICS = True
@@ -81,32 +88,18 @@ class ProductionConfig(BaseConfig):
     REMEMBER_COOKIE_DOMAIN = COOKIE_DOMAIN
     SESSION_COOKIE_DOMAIN = COOKIE_DOMAIN
 
-    # Resources
-    STORAGE_TYPE = env('DIVE_STORAGE_TYPE', 'file')
-    if STORAGE_TYPE == 'file':
-        STORAGE_PATH = base_dir_path('uploads')  # env('DIVE_STORAGE_PATH', '/usr/local/lib/dive')
-    else:
-        AWS_KEY_ID = env('DIVE_AWS_KEY_ID')
-        AWS_SECRET = env('DIVE_AWS_SECRET')
-        AWS_DATA_BUCKET = env('DIVE_AWS_DATA_BUCKET')
-
-    # DB
-    DATABASE_URI = '%s:%s@%s/%s' % (env('SQLALCHEMY_DATABASE_USER'), env('SQLALCHEMY_DATABASE_PASSWORD'), env('SQLALCHEMY_DATABASE_ENDPOINT'), env('SQLALCHEMY_DATABASE_NAME'))
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://%s' % DATABASE_URI
-
     # Analytics
     SENTRY_DSN = env('SENTRY_DSN')
     SENTRY_USER_ATTRS = [ 'username', 'email' ]
 
     # Worker
-    # https://www.cloudamqp.com/docs/celery.html
-    CELERY_BROKER_URL = env('DIVE_AMQP_URL')
-    CELERY_RESULT_BACKEND =  'db+postgresql://%s' % DATABASE_URI
     BROKER_POOL_LIMIT = 1 # Will decrease connection usage
     BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
     BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
     CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
     CELERY_EVENT_QUEUE_EXPIRES = 60
+
+    # Result persistence
     RECOMPUTE_FIELD_PROPERTIES = False
     RECOMPUTE_VIZ_SPECS = False
     RECOMPUTE_STATISTICS = False
@@ -131,14 +124,13 @@ class TestingConfig(BaseConfig):
     SENTRY_USER_ATTRS = [ 'username', 'email' ]
 
     # Worker
-    # https://www.cloudamqp.com/docs/celery.html
-    CELERY_BROKER_URL = env('DIVE_AMQP_URL')
-    CELERY_RESULT_BACKEND =  'db+postgresql://%s' % DATABASE_URI
     BROKER_POOL_LIMIT = 1 # Will decrease connection usage
     BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
     BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
     CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
     CELERY_EVENT_QUEUE_EXPIRES = 60
+
+    # Result persistence
     RECOMPUTE_FIELD_PROPERTIES = False
     RECOMPUTE_VIZ_SPECS = False
     RECOMPUTE_STATISTICS = False
