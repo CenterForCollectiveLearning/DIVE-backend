4486fd64bd6acabadffe5cba2c1b404cb02ace9e
kevinzenghu <kzh@mit.edu>
2016-11-07 10:43:45 -0500

    Add barebones fabric deploy script

--- fabfile.py
+++ fabfile.py
@@ -1,40 +1,26 @@
 from __future__ import with_statement
-from fabric.api import local, env, settings, abort, run, cd
+from fabric.api import local, env, settings, abort, run, cd, hosts
 from fabric.contrib.console import confirm
 import time
 
 env.use_ssh_config = True
-env.hosts = ['dive-server-large', 'dive-server-micro-1', 'dive-server-micro-2']
-env.user = 'ubuntu'
+api_hosts = ['dive-server-large', 'dive-server-micro-1']
+worker_hosts = ['dive-worker-aws']
 
-code_dir='/var/www/deploy-stage'
-app_dir='/var/www/application'
-repo='git@github.com:Servers-for-Hackers/deploy-ex.git'
-timestamp="release_%s" % int(time.time() * 1000)
+code_dir='~/DIVE-backend'
 
-def deploy():
+@hosts(api_hosts)
+def deploy_api():
     fetch_repo()
-    run_composer()
-    update_permissions()
-    update_symlinks()
+
+
+@hosts(worker_hosts)
+def deploy_worker():
+    fetch_repo()
 
 def fetch_repo():
     with cd(code_dir):
         with settings(warn_only=True):
-            run("mkdir releases")
-    with cd("%s/releases" % code_dir):
-        run("git clone %s %s" % (repo, timestamp))
-
-def run_composer():
-    with cd("%s/releases/%s" % (code_dir, timestamp)):
-        run("composer install --prefer-dist")
-
-def update_permissions():
-    with cd("%s/releases/%s" % (code_dir, timestamp)):
-        run("chgrp -R www-data .")
-        run("chmod -R ug+rwx .")
-
-def update_symlinks():
-    with cd(code_dir):
-        run("ln -nfs %s %s" % (code_dir+'/releases/'+timestamp, app_dir))
-        run("chgrp -h www-data %s" % app_dir)
+            run("source venv/bin/activate")
+            run("pip install -r requirements.txt")
+            run("git pull")
