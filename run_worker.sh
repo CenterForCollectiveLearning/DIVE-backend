0f3908520360eeb434725644f070f5e799c16fd9
kevinzenghu <kzh@mit.edu>
2016-11-14 18:39:14 -0500

    Using JSON not PJSON as default serializer

--- run_worker.sh
+++ run_worker.sh
@@ -2,7 +2,4 @@ celery worker \
   -E \
   --app=dive.worker.core \
   --autoscale=10,3 \
-  --loglevel=DEBUG \
-  # --without-gossip \
-  # --without-mingle \
-  # --without-heartbeat
+  --loglevel=DEBUG
