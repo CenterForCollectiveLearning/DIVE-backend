c78f2b15db8eea20289b7555e1270b5b0aba8b9d
kzh <kzh@mit.edu>
2017-03-17 18:08:12 -0400

    Date parser no more bug on multiple instances

--- dive/base/constants/data.py
+++ dive/base/constants/data.py
@@ -130,12 +130,12 @@ class DataTypeWeights(Enum):
     CONTINENT_NAME = 10
 
     # Temporal
-    DATETIME = 1
-    DATE = 1
-    TIME = 1
-    YEAR = 1
-    MONTH = 1
-    DAY = 1
+    DATETIME = 5
+    DATE = 5
+    TIME = 5
+    YEAR = 5
+    MONTH = 5
+    DAY = 5
 
 
 class PseudoType(Enum):
