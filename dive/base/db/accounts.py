57ec0deca5c9aa66494175da49ded3301f5853e2
kzh <kzh@mit.edu>
2017-03-20 10:38:47 -0400

    Small debug in project auth

--- dive/base/db/accounts.py
+++ dive/base/db/accounts.py
@@ -35,7 +35,7 @@ def project_auth(project_id):
 def is_authorized_user(current_user, matching_project):
     logger.info('Global admin: %s', current_user.is_global_admin())
     logger.info('Matching project private: %s', matching_project.private)
-    logger.info('Matching project id: %s, %s', matching_project.user_id, current_user.id)
+    logger.info('Matching project id: %s, %s (%s) (%s)', matching_project.user_id, current_user.id, (matching_project.user_id == current_user.id), (matching_project.user_id is current_user.id))
     if current_user.is_global_admin() or not matching_project.private:
         return True
 
