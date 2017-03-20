a884c2fc1af1f1ddc765733006c871c9d04c20dc
kzh <kzh@mit.edu>
2017-03-20 10:27:12 -0400

    Tweaking anonymous user confirmation

--- fabfile.py
+++ fabfile.py
@@ -4,23 +4,28 @@ from fabric.contrib.console import confirm
 import time
 
 env.use_ssh_config = True
-api_hosts = ['dive-server-large', 'dive-server-micro-1']
+api_hosts = ['dive-api-aws']
 worker_hosts = ['dive-worker-aws']
 
 code_dir='~/DIVE-backend'
 
+
 @hosts(api_hosts)
 def deploy_api():
     fetch_repo()
-
+    run("tmux a -t dive")
+    run("pkill gunicorn")
+    run("source venv/bin/activate")
+    run("sh run_server.sh")
 
 @hosts(worker_hosts)
 def deploy_worker():
     fetch_repo()
 
+
 def fetch_repo():
     with cd(code_dir):
         with settings(warn_only=True):
             run("source venv/bin/activate")
-            run("pip install -r requirements.txt")
+            # run("pip install -r requirements.txt")
             run("git pull")
