533fcc67aa9bac330becaef24487f77be45eec16
kzh <kzh@mit.edu>
2017-07-25 17:18:27 -0400

    Fix comparison return data

--- dive/worker/statistics/comparison/pipelines.py
+++ dive/worker/statistics/comparison/pipelines.py
@@ -19,31 +19,34 @@ def run_comparison_from_spec(spec, project_id, conditionals=[]):
     all_fields = db_access.get_field_properties(project_id, dataset_id)
     dependent_variables = [ f for f in all_fields if f['name'] in dependent_variables_names ]
     independent_variables = [ f for f in all_fields if f['name'] in independent_variables_names ]
-    can_run_numerical_comparison_independent = len([ iv for iv in independent_variables if iv['general_type'] == 'q' ]) >= 2 and len(dependent_variables_names) == 0
-    can_run_numerical_comparison_dependent = len([ dv for dv in dependent_variables if dv['general_type'] == 'q' ]) >= 2 and len(independent_variables_names) == 0
+
+    can_run_numerical_comparison_independent = len([ iv for iv in independent_variables if iv['scale'] == 'continuous' ]) >= 2 and len(dependent_variables_names) == 0
+    can_run_numerical_comparison_dependent = len([ dv for dv in dependent_variables if dv['scale'] == 'continuous' ]) >= 2 and len(independent_variables_names) == 0
     can_run_numerical_comparison = (can_run_numerical_comparison_dependent or can_run_numerical_comparison_independent)
-    can_run_anova = len(dependent_variables) and len(independent_variables)
+
+    can_run_anova = (len(dependent_variables) and len(independent_variables))
 
     df = get_data(project_id=project_id, dataset_id=dataset_id)
     df_conditioned = get_conditioned_data(project_id, dataset_id, df, conditionals)
     df_subset = df_conditioned[ dependent_variables_names + independent_variables_names ]
-    df_ready = df_subset.dropna(how='all')  # Remove unclean
+    df_ready = df_subset.dropna(how='any')  # Remove unclean
 
     result = {}
     if can_run_anova:
         anova = run_anova(df_ready, independent_variables_names, dependent_variables_names)
-        anova_boxplot_data = get_anova_boxplot_data(project_id, dataset_id, df, independent_variables_names, dependent_variables_names)
-        pairwise_comparison_data = get_pairwise_comparison_data(df, independent_variables_names, dependent_variables_names, significance_cutoff=significance_cutoff)
+        anova_boxplot_data = get_anova_boxplot_data(project_id, dataset_id, df_ready, independent_variables_names, dependent_variables_names)
+        pairwise_comparison_data = get_pairwise_comparison_data(df_ready, independent_variables_names, dependent_variables_names, significance_cutoff=significance_cutoff)
         result.update({
             'anova': anova,
             'anova_boxplot': anova_boxplot_data,
             'pairwise_comparison': pairwise_comparison_data,
         })
+
     if can_run_numerical_comparison:
         if can_run_numerical_comparison_independent:
-            numerical_comparison_data = run_valid_numerical_comparison_tests(df, independent_variables_names, independence=True)
+            numerical_comparison_data = run_valid_numerical_comparison_tests(df_ready, independent_variables_names, independence=True)
         if can_run_numerical_comparison_dependent:
-            numerical_comparison_data = run_valid_numerical_comparison_tests(df, dependent_variables_names, independence=False)
+            numerical_comparison_data = run_valid_numerical_comparison_tests(df_ready, dependent_variables_names, independence=False)
         result['numerical_comparison'] = numerical_comparison_data
 
     return result, 200
